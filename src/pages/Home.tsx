bbe8b94e9ec6df1887a0566322b4a5fe10220970
WordsPerMinute <git@coreyhodge.net>
2020-07-19 21:55:29 -0600

    added favicon

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,23 +1,39 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import React from 'react';
+import { 
+  IonContent, 
+  IonHeader, 
+  IonPage, 
+  IonTitle, 
+  IonToolbar, 
+  IonButton, 
+  IonIcon, 
+  IonToast 
+} from '@ionic/react';
+import { fastFoodOutline } from 'ionicons/icons';
+import React, { useState } from 'react';
 import ExploreContainer from '../components/ExploreContainer';
 import './Home.css';
 
 const Home: React.FC = () => {
+
+  const [showToast, setShowToast] = useState(false);
+  const handleClick = () => {
+    setShowToast(true);
+    setTimeout(() => setShowToast(false), 1500)
+  };
+
   return (
     <IonPage>
       <IonHeader>
         <IonToolbar>
-          <IonTitle>Blank</IonTitle>
+          <IonTitle>PlateTrader</IonTitle>
         </IonToolbar>
       </IonHeader>
-      <IonContent>
-        <IonHeader collapse="condense">
-          <IonToolbar>
-            <IonTitle size="large">Blank</IonTitle>
-          </IonToolbar>
-        </IonHeader>
-        <ExploreContainer />
+      <IonContent className="ion-padding">
+        <IonButton onClick={handleClick}>
+          <IonIcon icon={fastFoodOutline} slot="start"/>
+          Sign Up
+        </IonButton>
+        <IonToast isOpen={showToast} message="popping up!"></IonToast>
       </IonContent>
     </IonPage>
   );
